df59d20092063f0f2087c571ff17c83186b76b83
rustydb <doomslayer@hpe.com>
2021-11-23 11:36:47 -0600

    MTL-1560 Support Single Disk RAIDs
    This fixes a bug that was discovered while using a single disk RAID (for
    use in virtual or lab testing). This fixes an early exit, if multiple
    candidates are found and the first one was insufficient the disk scanner
    would exit early. This change ensures that we're always checking all
    possible candidates.

--- 93metaldmk8s/metal-dmk8s-disks.sh
+++ 93metaldmk8s/metal-dmk8s-disks.sh
@@ -6,19 +6,20 @@ ls /dev/sd* > /dev/null 2>&1 || exit 1
 
 # SUBBROUTINES
 type make_ephemeral > /dev/null 2>&1 || . /lib/metal-dmk8s-lib.sh
+type metal_resolve_disk > /dev/null 2>&1 || . /lib/metal-lib.sh
 
 # DISKS or RETRY
-# Ignore whatever was selected for the overlay by starting +1 from that index.
-disk_index=$((${metal_disks:-2} + 1))
-ephemeral="$(lsblk -b -l -o SIZE,NAME,TYPE,TRAN | grep -E '(sata|nvme|sas)' | sort -h | awk '{print $1 " " $2}' | tail -n +${disk_index} | tr '\n' ' ')"
+# Offset the selection; choose any disk that wasn't selected by the RAID.
+disk_offset=$((${metal_disks:-2} + 1))
+ephemeral="$(lsblk -b -l -o SIZE,NAME,TYPE,TRAN | grep -E '(sata|nvme|sas)' | sort -h | awk '{print $1 "," $2}' | tail -n +${disk_offset} | tr '\n' ' ')"
 [ -z "${ephemeral}" ] && exit 1
-ephemeral_size="$(echo $ephemeral | awk '{print $1}')"
+
+# Find the right disk.
 # 1048576000000 is 1 TiB; required for this disk.
 # exit 0 ; this module does not need to run on this node unless it meets the requirements.
-[ "${ephemeral_size}" -lt 1048576000000 ] && exit 0
+ephemeral_disk=$(metal_resolve_disk "$ephemeral" 1048576000000) || exit 0
 
-# DISKS
-ephemeral_disk="$(echo $ephemeral | awk '{print $2}')"
+# Process the disk.
 if [ "${metal_nowipe:-0}" = 0 ]; then
     [ ! -f /tmp/metalephemeraldisk.done ] && make_ephemeral "$ephemeral_disk"
 fi
