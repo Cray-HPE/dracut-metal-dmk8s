8487dc9cd53e4367e2903668589e1ef25c6dc63c
Russell Bunch <doomslayer@hpe.com>
2021-12-02 11:19:46 -0600

    MTL-1561 adjust sorting to cease grabbing GCP partitions
    This reverts commit 589e6ba2c5f01aa64492515029d4267240253ea0 with fixes
    for omitting NVME partitions on GCP nodes.

--- 93metaldmk8s/metal-dmk8s-disks.sh
+++ 93metaldmk8s/metal-dmk8s-disks.sh
@@ -10,8 +10,9 @@ type metal_resolve_disk > /dev/null 2>&1 || . /lib/metal-lib.sh
 
 # DISKS or RETRY
 # Offset the selection; choose any disk that wasn't selected by the RAID.
+# MAINTAINER NOTE: Regardless of gcp mode, this will ignore any NVME partition incase they stick around after wiping.
 disk_offset=$((${metal_disks:-2} + 1))
-ephemeral="$(lsblk -b -l -o SIZE,NAME,TYPE,TRAN | grep -E '('"$metal_transports"')' | sort -u | awk '{print $1 "," $2}' | tail -n +${disk_offset} | tr '\n' ' ' | sed 's/ *$//')"
+ephemeral="$(lsblk -b -l -o SIZE,NAME,TYPE,TRAN | grep -E '('"$metal_transports"')' | sort -h | grep -vE 'p[0-9]+$' | awk '{print $1 "," $2}' | tail -n +${disk_offset} | tr '\n' ' ' | sed 's/ *$//')"
 [ -z "${ephemeral}" ] && exit 1
 
 # Find the right disk.
