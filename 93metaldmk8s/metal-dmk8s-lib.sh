87bd3e6d46ea0940d59b35b512ab2d939a7d467c
Russell Bunch <doomslayer@hpe.com>
2022-06-21 13:07:04 -0500

    MTL-1288 Fix Race Condition and Debug Mode (#19)
    * MTL-1288 Fix Race Condition and Debug Mode
    
    This fixes two race conditions.
    - Fix the race condition with the pave function. Do not create
    anything until after the pave function has ran to completion.
    - Fix the race condition with dmsquash-live, add wait_for_dev
    to prevent pivoting to root before dmk8s finishes.
    
    This also fixes debug. metal.debug was not honoring metal.debug=1 in some
    cases. Also add metal.debug to every possible file except for
    module-setup.sh.
    
    This change removes the need to key off metal.no-wipe. Instead if the
    ephemeral disk is found this module quits, otherwise if luks is disabled
    and a deployment server is set this module must find a disk.
    metal.no-wipe is just a safeguard to ensure wipe functions are not
    invoked when it's enabled. This makes behavior consistent with
    dracut-metal-mdsquash.
    
    Leverage the new disks_exist method to wait for /dev/sd or /dev/nvme
    devices. This ensures that pure NVME systems can deploy, otherwise only
    SD systems and mixed (SD + NVME) are compatible.
    
    Add/update License
    
    * MTL-1497 Add Shellcheck

--- 93metaldmk8s/metal-dmk8s-lib.sh
+++ 93metaldmk8s/metal-dmk8s-lib.sh
@@ -1,15 +1,79 @@
 #!/bin/bash
+#
+# MIT License
+#
+# (C) Copyright 2022 Hewlett Packard Enterprise Development LP
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# metal-dmk8s-lib.sh
+[ "${metal_debug:-0}" = 0 ] || set -x
 
-type info > /dev/null 2>&1 || . /lib/dracut-lib.sh
+export EPHEMERAL_DONE_FILE=/tmp/metalephemeraldisk.done
 
-metal_conrun=$(getarg metal.disk.conrun)
-[ -z "${metal_conrun}" ] && metal_conrun=LABEL=CONRUN
-metal_conlib=$(getarg metal.disk.conlib)
-[ -z "${metal_conlib}" ] && metal_conlib=LABEL=CONLIB
-metal_k8slet=$(getarg metal.disk.k8slet)
-[ -z "${metal_k8slet}" ] && metal_k8slet=LABEL=K8SLET
+command -v info > /dev/null 2>&1 || . /lib/dracut-lib.sh
 
+##############################################################################
+# function: scan_ephemeral
+#
+# Creates a "done" file if the ephemeral disk and its partitions exist.
+#
+scan_ephemeral() {
+    local conrun_scheme=${metal_conrun%=*}
+    local conrun_authority=${metal_conrun#*=}
+    local conlib_scheme=${metal_conlib%=*}
+    local conlib_authority=${metal_conlib#*=}
+    local k8slet_scheme=${metal_k8slet%=*}
+    local k8slet_authority=${metal_k8slet#*=}
+    local disks=()
+    disks+=( "/dev/disk/by-${conrun_scheme,,}/${conrun_authority^^}" )
+    disks+=( "/dev/disk/by-${conlib_scheme,,}/${conlib_authority^^}" )
+    disks+=( "/dev/disk/by-${k8slet_scheme,,}/${k8slet_authority^^}" )
+    for disk in "${disks[@]}"; do 
+        if blkid -s UUID -o value "$disk" >/dev/null; then
+    
+            # echo 0 to signal that nothing was done; the disks exist
+            echo 0 > $EPHEMERAL_DONE_FILE
+        else
+            # A disk doesn't exist yet.
+            # Remove the file if it was created and then exit the loop to start making the disks.
+            rm -f $EPHEMERAL_DONE_FILE
+            break
+        fi
+    done
+}
+
+##############################################################################
+# function: make_ephemeral
+#
+# Returns 0 if a disk was partitioned, otherwise this calls 
+# metal_die with a contextual error message.
+# 
+# Requires 1 argument for which disk:
+#
+#   sda
+#   nvme0
+#
+# NOTE: The disk name must be given without any partitions or `/dev` prefixed
+#       paths.
 make_ephemeral() {
+
     local target="${1:-}" && shift
     [ -z "$target" ] && info 'No ephemeral disk.' && return 0
 
@@ -23,13 +87,9 @@ make_ephemeral() {
         target="${target}p" 
     fi
 
-    # NOTE: These don't persist, but are appropriate to add in-case they ever do.
-    sleep 2
-    mkfs.xfs -f -L ${metal_conrun#*=} "/dev/${target}1" || warn Failed to create "${metal_conrun#*=}"
-    sleep 2
-    mkfs.xfs -f -L ${metal_conlib#*=} "/dev/${target}2" || warn Failed to create "${metal_conlib#*=}"
-    sleep 2
-    mkfs.xfs -f -L ${metal_k8slet#*=} "/dev/${target}3" || warn Failed to create "${metal_k8slet#*=}"
+    mkfs.xfs -f -L ${metal_conrun#*=} "/dev/${target}1" || metal_dmk8s_die "Failed to create ${metal_conrun#*=}"
+    mkfs.xfs -f -L ${metal_conlib#*=} "/dev/${target}2" || metal_dmk8s_die "Failed to create ${metal_conlib#*=}"
+    mkfs.xfs -f -L ${metal_k8slet#*=} "/dev/${target}3" || metal_dmk8s_die "Failed to create ${metal_k8slet#*=}"
 
     mkdir -p /run/containerd /var/lib/kubelet /var/lib/containerd /run/lib-containerd
     {
@@ -44,5 +104,18 @@ make_ephemeral() {
     # Mount FS again, catching our new overlayFS. Failure to mount here is fatal.
     mount -a -v -T $metal_fstab
 
-    echo 1 > /tmp/metalephemeraldisk.done && return
+    # echo 1 to signal that this module create a disk.
+    echo 1 > $EPHEMERAL_DONE_FILE && return
+}
+
+
+##############################################################################
+# function: metal_dmk8s_die
+#
+# Calls metal_die, printing this module's URL to its source code first.
+#
+metal_dmk8s_die() {
+    command -v metal_die > /dev/null 2>&1 || . /lib/metal-lib.sh
+    echo >&2 "GitHub/Docs: https://github.com/Cray-HPE/dracut-metal-dmk8s"
+    metal_die $*
 }
--- 93metaldmk8s/parse-metal-dmk8s.sh
+++ 93metaldmk8s/parse-metal-dmk8s.sh
@@ -1,25 +1,53 @@
 #!/bin/bash
 #
-# Copyright 2020 Hewlett Packard Enterprise Development LP
+# MIT License
 #
-# live.overlay.readonly: Set to mark the entire overlayFS as read-only.
-# > metal.disk.conrun.size=
+# (C) Copyright 2022 Hewlett Packard Enterprise Development LP
 #
-# live.dir: This can change where the squashFS is stored within its storage.
-# > rd.live.dir=LiveOS
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
 #
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# parse-metal-dmk8s.sh
+
 type getarg > /dev/null 2>&1 || . /lib/dracut-lib.sh
 
 getargbool 0 metal.debug -d -y metal_debug && metal_debug=1
-# Afixed size:
+[ "${metal_debug:-0}" = 0 ] || set -x
+
+metal_conrun=$(getarg metal.disk.conrun)
+[ -z "${metal_conrun}" ] && metal_conrun=LABEL=CONRUN
+metal_conlib=$(getarg metal.disk.conlib)
+[ -z "${metal_conlib}" ] && metal_conlib=LABEL=CONLIB
+metal_k8slet=$(getarg metal.disk.k8slet)
+[ -z "${metal_k8slet}" ] && metal_k8slet=LABEL=K8SLET
+
+# Affixed siz(s):
 metal_size_conrun=$(getargnum 75 10 150 metal.disk.conrun.size)
-# Percentages as sizes
+# Percentages as size(s):
 metal_size_conlib=$(getargnum 40 10 45 metal.disk.conlib.size)
 metal_size_k8slet=$(getargnum 10 10 45 metal.disk.k8slet)
 
 getargbool 0 metal.no-wipe -d -y metal_nowipe && metal_nowipe=1 || metal_nowipe=0
 
 export metal_debug
+export metal_conrun
+export metal_conlib
+export metal_k8slet
 export metal_size_conrun
 export metal_size_conlib
 export metal_size_k8slet
