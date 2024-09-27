313d19c0eecd0c7662f9af3ba4b7cae97f06355a
Russell Bunch <doomslayer@hpe.com>
2024-09-27 16:34:42 -0500

    shfmt and `.editorconfig`

--- 93metaldmk8s/module-setup.sh
+++ 93metaldmk8s/module-setup.sh
@@ -26,32 +26,32 @@
 
 # called by dracut
 check() {
-    require_binaries blkid lsblk mkfs.xfs mount parted partprobe || return 1
-    return 
+  require_binaries blkid lsblk mkfs.xfs mount parted partprobe || return 1
+  return
 }
 
 # called by dracut
 depends() {
-    # Needed so overlayFS is available, that's where the new fstab is dropped.
-    echo metalmdsquash
-    return 0
+  # Needed so overlayFS is available, that's where the new fstab is dropped.
+  echo metalmdsquash
+  return 0
 }
 
 installkernel() {
-    instmods hostonly=''
+  instmods hostonly=''
 }
 
 # called by dracut
 install() {
-    # the rest is needed by metal-squashfs-url-dract.
-    # rmdir is needed by dmsquash-live/livenet
-    inst_multiple chmod lsblk mkfs.xfs parted partprobe sort tail
+  # the rest is needed by metal-squashfs-url-dract.
+  # rmdir is needed by dmsquash-live/livenet
+  inst_multiple chmod lsblk mkfs.xfs parted partprobe sort tail
 
-    # shellcheck disable=SC2154
-    inst_simple "$moddir/metal-dmk8s-lib.sh" "/lib/metal-dmk8s-lib.sh"
-    inst_script "$moddir/metal-dmk8s-disks.sh" /sbin/metal-dmk8s-disks
+  # shellcheck disable=SC2154
+  inst_simple "$moddir/metal-dmk8s-lib.sh" "/lib/metal-dmk8s-lib.sh"
+  inst_script "$moddir/metal-dmk8s-disks.sh" /sbin/metal-dmk8s-disks
 
-    inst_hook cmdline 10 "$moddir/parse-metal-dmk8s.sh"
-    inst_hook pre-udev 10 "$moddir/metal-dmk8s-genrules.sh"
-    dracut_need_initqueue
+  inst_hook cmdline 10 "$moddir/parse-metal-dmk8s.sh"
+  inst_hook pre-udev 10 "$moddir/metal-dmk8s-genrules.sh"
+  dracut_need_initqueue
 }
